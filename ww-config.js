4c5e3bb6d16364933eb0360e33c52ab750ac6bd7
SebWell <sebastien.mignot@gmail.com>
2025-07-01 02:53:27 +0200

    FIX: Add complete Weweb configuration with all component options

--- ww-config.js
+++ ww-config.js
@@ -1,6 +1,6 @@
 export default {
     editor: {
-        label: 'card',
+        label: 'Shadcn Card',
         icon: 'square',
     },
     triggerEvents: [
@@ -8,11 +8,105 @@ export default {
     ],
     properties: {
         content: {
-            label: 'Content',
+            label: 'Card Settings',
             type: 'Object',
             section: 'settings',
             bindable: true,
-            defaultValue: {}
+            defaultValue: {
+                showHeader: true,
+                title: '',
+                description: '',
+                bodyContent: '',
+                showFooter: false,
+                footerContent: '',
+                headerPadding: 'default',
+                contentPadding: 'default',
+                footerPadding: 'default',
+                customClasses: ''
+            },
+            options: {
+                item: {
+                    showHeader: {
+                        label: 'Show Header',
+                        type: 'OnOff',
+                        section: 'settings',
+                        defaultValue: true
+                    },
+                    title: {
+                        label: 'Title',
+                        type: 'Text',
+                        section: 'settings',
+                        bindable: true,
+                        hidden: (content) => !content.showHeader,
+                        defaultValue: ''
+                    },
+                    description: {
+                        label: 'Description',
+                        type: 'LongText',
+                        section: 'settings',
+                        bindable: true,
+                        hidden: (content) => !content.showHeader,
+                        defaultValue: ''
+                    },
+                    bodyContent: {
+                        label: 'Body Content',
+                        type: 'LongText',
+                        section: 'settings',
+                        bindable: true,
+                        defaultValue: ''
+                    },
+                    showFooter: {
+                        label: 'Show Footer',
+                        type: 'OnOff',
+                        section: 'settings',
+                        defaultValue: false
+                    },
+                    footerContent: {
+                        label: 'Footer Content',
+                        type: 'LongText',
+                        section: 'settings',
+                        bindable: true,
+                        hidden: (content) => !content.showFooter,
+                        defaultValue: ''
+                    },
+                    headerPadding: {
+                        label: 'Header Padding',
+                        type: 'TextSelect',
+                        section: 'style',
+                        options: {
+                            choices: [
+                                { value: 'none', label: 'None' },
+                                { value: 'sm', label: 'Small' },
+                                { value: 'default', label: 'Default' },
+                                { value: 'lg', label: 'Large' }
+                            ]
+                        },
+                        defaultValue: 'default'
+                    },
+                    contentPadding: {
+                        label: 'Content Padding',
+                        type: 'TextSelect',
+                        section: 'style',
+                        options: {
+                            choices: [
+                                { value: 'none', label: 'None' },
+                                { value: 'sm', label: 'Small' },
+                                { value: 'default', label: 'Default' },
+                                { value: 'lg', label: 'Large' },
+                                { value: 'full', label: 'Full' }
+                            ]
+                        },
+                        defaultValue: 'default'
+                    },
+                    customClasses: {
+                        label: 'Custom Classes',
+                        type: 'Text',
+                        section: 'style',
+                        bindable: true,
+                        defaultValue: ''
+                    }
+                }
+            }
         }
     }
 };
